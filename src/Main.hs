f64ce7233346bc4935ffb3e315b11e764299ecfc
Roy Blankman <riblankman@gmail.com>
2015-01-27 13:30:15 -0800

    working on some crosses

--- src/Main.hs
+++ src/Main.hs
@@ -4,15 +4,22 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG
 import Diagrams.TwoD.Size
 
-verticalBar = square 1 # scaleX 0.3
-horizontalBar = square 1 # scaleY 0.3
+blackFill = fc black # lw none
+sourceBar = square 1 # scaleY 0.1 # blackFill
+
+horizontalBar = sourceBar # translateY 0.18
+verticalBar = sourceBar # rotateBy (1/4)
+footBar = sourceBar # scaleX 0.5 # rotateBy (1/8) # translateY (-0.2)
+inriBar = sourceBar # scaleX 0.5 # translateY 0.3
 
 iconSize = mkSizeSpec (Just 120) Nothing
 
 cross = verticalBar `atop` horizontalBar
 anticross = cross # scaleY (-1)
-orthodoxcross = verticalBar `atop` horizontalBar
+orthodoxCross = verticalBar `atop` horizontalBar `atop` footBar
+orthodoxCrossWithInri = verticalBar `atop` horizontalBar `atop` footBar `atop` inriBar
 
 main = do renderSVG "svg/cross.svg" iconSize (cross :: Diagram B R2)
           renderSVG "svg/anticross.svg" iconSize (anticross :: Diagram B R2)
-          renderSVG "svg/orthodoxcross.svg" iconSize (orthodoxcross :: Diagram B R2)
+          renderSVG "svg/orthodox-cross.svg" iconSize (orthodoxCross :: Diagram B R2)
+          renderSVG "svg/orthodox-cross-with-inri.svg" iconSize (orthodoxCrossWithInri :: Diagram B R2)
